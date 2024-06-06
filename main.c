ae9f1e64f82d2b8677e953810eec387d538ff951
Yogev Arye <106072953+YogevArye@users.noreply.github.com>
2024-06-06 18:50:45 +0300

    Update main.c

--- main.c
+++ main.c
@@ -3,12 +3,12 @@
 unsigned ReverseNumber(unsigned number) {
     unsigned ones_digit, tens_digit, hundreds_digit, result;
 
-    // Extract the digits in the correct order
+    /*Extract the digits in the correct order*/
     hundreds_digit = number / 100;        
     tens_digit = (number / 10) % 10;      
     ones_digit = number % 10;             
 
-    // Combine the digits in reverse order
+    /* Combine the digits in reverse order*/
     result = ones_digit * 100 + tens_digit * 10 + hundreds_digit;
 
     return result;
@@ -17,7 +17,7 @@ unsigned ReverseNumber(unsigned number) {
 int main() {
    
     unsigned number_to_reverse = 123;
-    printf("Reversed number: %d\n", ReverseNumber(number_to_reverse));
+    printf("Reversed number: %d\n", ReverseNumber(number_to_reverse));/*Expected value: 321*/
     
 
 
